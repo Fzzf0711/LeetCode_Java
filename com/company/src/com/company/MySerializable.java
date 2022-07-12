2650a326d14c7d5720c1ce1f39ec1b73257076b7
fzzf0711 <1326176537@qq.com>
2022-07-13 00:55:00 +0800

    format code

--- com/company/src/com/company/MySerializable.java
+++ com/company/src/com/company/MySerializable.java
@@ -3,47 +3,44 @@ package com.company;
 import java.io.*;
 
 public class MySerializable implements Serializable {
-    private int age;
-    //名字
-    @Serial
-    private  static  final  long serialVersionUID=1;
-    private String name ;
+  private int age;
+  // 名字
+  @Serial private static final long serialVersionUID = 1;
+  private String name;
 
-    public MySerializable() {
-    }
+  public MySerializable() {}
 
-    public int getAge() {
-        return age;
-    }
-    public void setAge(int age) {
-        this.age = age;
-    }
+  public int getAge() {
+    return age;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public void setAge(int age) {
+    this.age = age;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        FileOutputStream fileOutputStream = new FileOutputStream("temp.txt");
-        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-        MySerializable mySerializable = new MySerializable();
+  public void setName(String name) {
+    this.name = name;
+  }
 
-        mySerializable.setAge(19);
-        mySerializable.setName("saf");
+  public static void main(String[] args) throws IOException, ClassNotFoundException {
+    FileOutputStream fileOutputStream = new FileOutputStream("temp.txt");
+    ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+    MySerializable mySerializable = new MySerializable();
 
-        objectOutputStream.writeObject(mySerializable);
-        objectOutputStream.flush();
-        objectOutputStream.close();
+    mySerializable.setAge(19);
+    mySerializable.setName("saf");
 
-        FileInputStream fis = new FileInputStream("temp.txt");
-        ObjectInputStream oin = new ObjectInputStream(fis);
-        MySerializable user = (MySerializable) oin.readObject();
-        System.out.println("name="+user.getName());
-    }
+    objectOutputStream.writeObject(mySerializable);
+    objectOutputStream.flush();
+    objectOutputStream.close();
 
-
+    FileInputStream fis = new FileInputStream("temp.txt");
+    ObjectInputStream oin = new ObjectInputStream(fis);
+    MySerializable user = (MySerializable) oin.readObject();
+    System.out.println("name=" + user.getName());
+  }
 }

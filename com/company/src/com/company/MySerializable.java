5684aed06518d147b89f38a456c8dd88740c56ab
fzzf0711 <1326176537@qq.com>
2022-07-13 00:29:49 +0800

    Serilalizable test

---
+++ com/company/src/com/company/MySerializable.java
@@ -0,0 +1,49 @@
+package com.company;
+
+import java.io.*;
+
+public class MySerializable implements Serializable {
+    private int age;
+    //名字
+    @Serial
+    private  static  final  long serialVersionUID=1;
+    private String name ;
+
+    public MySerializable() {
+    }
+
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        FileOutputStream fileOutputStream = new FileOutputStream("temp.txt");
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+        MySerializable mySerializable = new MySerializable();
+
+        mySerializable.setAge(19);
+        mySerializable.setName("saf");
+
+        objectOutputStream.writeObject(mySerializable);
+        objectOutputStream.flush();
+        objectOutputStream.close();
+
+        FileInputStream fis = new FileInputStream("temp.txt");
+        ObjectInputStream oin = new ObjectInputStream(fis);
+        MySerializable user = (MySerializable) oin.readObject();
+        System.out.println("name="+user.getName());
+    }
+
+
+}
